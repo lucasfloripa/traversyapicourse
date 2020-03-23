4e086dd95c5003e559ad4b7361adab3b7fe4dbfc
Lucas Gonçalves <lucas.floripa@icloud.com>
2020-03-23 20:59:19 -0300

    Create routes and controllers folder

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
 const express = require("express");
 const dontenv = require("dotenv");
 
+// Route files
+const bootcamps = require("./routes/bootcamps");
+
 // Load env vars
 dontenv.config({ path: "./config/config.env" });
 
 // Init express
 const app = express();
+
+app.use("/api/v1/bootcamps", bootcamps);
+
+// Creating Routes
+// app.get("/", (req, res) => {
+//   res.send("Hello Express!");
+//   res.json({ name: "Lucas "});
+//   res.sendStatus(400 or any other http status);
+//   res.status(200).json({ success: false, data: { id: "1"}});
+// });
 
 const PORT = process.env.PORT;
 
